4abcdf6687474039ff5790fe7128093c731a3975
alinavodo <alinavodo@gmail.com>
2023-12-25 23:31:11 +0300

    feat: completed task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let res;
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  if(isNaN(sampleActivity) || !sampleActivity) {
+    res = 'false';
+  } else {
+    res = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k;
+   
+  }
+  return res;
 }
 
 module.exports = {
